791f241a253f50c845e60a8359c147e2818e003d
Cecilia Bodin <cecilia.bodin@gmail.com>
2014-02-08 17:11:07 -0500

    adding the form

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -2,4 +2,20 @@ class PicsController < ApplicationController
 	def index
 		@pics = Pic.all
 	end
+
+	def new 
+		@pic = Pic.new
+	end
+
+	def create
+		Pic.create(pics_params)
+		redirect_to pics_path
+	end
+
+	private 
+
+	def pics_params
+		params.require(:pic).permit(:hour, :emotion, :learned)
+	end
+
 end
